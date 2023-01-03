a693cae087500802ac959ca98c165da1c46efa3f
evgeniy <shtorm20300@gmail.com>
2023-01-03 21:39:31 +0600

    сумма от м до н

---
+++ z2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Write("Введите минимально число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(SumIntegers(num1, num2));
+int SumIntegers(int m, int n)
+{
+    if (m == n)
+    {
+        return m;
+    }
+    else
+    {
+        return m + SumIntegers(m + 1, n);
+    }
+}
